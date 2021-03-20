5d2c33a2e66a56c506cbd9f4e8bc9a1d531f94f8
matleg <ml971@hotmail.com>
2021-03-20 23:35:09 +0100

    add comments and clean

--- steganograformerge.py
+++ steganograformerge.py
@@ -8,22 +8,6 @@ from PIL import ExifTags
 FILE_PATH = Path(__file__).absolute()
 FILE_DIR = Path().absolute()
 
-RED = 0
-GREEN = 1
-BLUE = 2
-
-
-def get_images_paths(directory=FILE_DIR):
-    """
-    """
-    images = []
-    for img in os.listdir(directory):
-        if not os.path.isfile(img):
-            continue
-        if imghdr.what(img) in ('png', 'jpeg'):
-            images.append(img)
-    return images
-
 
 def get_image(image_name, directory=FILE_DIR):
     """
@@ -35,47 +19,11 @@ def get_image(image_name, directory=FILE_DIR):
     return img
 
 
-def print_image_properties(image):
-    """
-    input: Image object
-    """
-    print(image.filename, ", format:", image.format, ", size:", image.size, ", mode:", image.mode)
-
-
-def print_images_properties(directory=FILE_DIR):
-    """
-    """
-    for image_path in get_images_paths(directory):
-        im = get_image(image_path)
-        print_image_properties(im)
-
-
-def print_tags(exifdata, max_size=200):
-    """
-    """
-
-    print("Tags in exifdata :")
-    for tag_id in exifdata:
-        tag = ExifTags.TAGS.get(tag_id)  # see exif_tags_list.txt
-        data = exifdata.get(tag_id, tag_id)  # take the number if no value associated
-        # do not print image
-        # if isinstance(data, bytes):
-        #     continue
-        # do not print large GPS coord
-        size = sys.getsizeof(data)
-        if size > max_size:
-            print(f"{tag_id:25}: Data too large ({size/1000}ko). tag type: {type(tag)}, data type: {type(data)}")
-            continue
-        print(f"{tag:25}: {data}, type: {type(data)}")
-    print("** End print tags **")
-
-
 def hide_image(photo, image_to_hide, significant_bits=1):
     """
+    Hide image in photo
     photo: Image object
     image: Image object
-    text_to_hide: ascii text to encode
-    color to choose
     1 pixel <==> 1 bit  (Not 3, one color only to choose)
     """
 
@@ -94,28 +42,25 @@ def hide_image(photo, image_to_hide, significant_bits=1):
             pr, pg, pb = photo.getpixel((x, y))
             ir, ig, ib = image_to_hide.getpixel((x, y))
 
+            # example with 3 most significant bits
+            # photo : 10101010 -> shift by 3 to right : 10101 -> shift by 3 to left : 10101000
+            # image : 11100000 -> shift by (8-3=5) to right : 111
+            # merge the two : 10101000 | 00000111 = 10101111
+
             nr = ((pr >> significant_bits) << significant_bits) | (ir >> non_significant_bits)
             ng = ((pg >> significant_bits) << significant_bits) | (ig >> non_significant_bits)
             nb = ((pb >> significant_bits) << significant_bits) | (ib >> non_significant_bits)
 
-            if x==0 and y==0:
-                print(bin((pr >> significant_bits) << significant_bits))
-                print(bin((ir >> non_significant_bits)))
-                print(((pr >> significant_bits) << significant_bits) & (ir >> non_significant_bits))
             new_photo.putpixel((x, y), (nr, ng, nb))
 
     return new_photo
-    
-    
+
+
 def save_picture(picture, filename):
-    """
-    """
     picture.save(filename)
 
 
 def main():
-    get_images_paths()
-    print_images_properties()
 
     photo_name = "Tower.jpg"
     image_name = "text_foobar.jpg"
@@ -123,22 +68,9 @@ def main():
     photo = get_image(photo_name)
     image = get_image(image_name)
 
-    print("photo")
-    data = photo.getdata()
-    imgdata = list(data)
-    for i in range(5):
-        print(imgdata[i * 10:i * 10 + 10])
-
-    print("image")
-    data = image.getdata()
-    imgdata = list(data)
-    for i in range(5):
-        print(imgdata[i * 10:i * 10 + 10])
-
-    
-    for significant_bits in (1, 2, 3, 4, 5, 6, 7, 8):
+    for significant_bits in range(1, 9):
         new_photo = hide_image(photo, image, significant_bits)
-        sb = str(significant_bits) 
+        sb = str(significant_bits)
         save_picture(new_photo, "tower_with_text_" + sb + "_sb.jpg")
 
 
